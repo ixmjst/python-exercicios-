ce1d3983ba6cc4f6f12acd6ea09f4dba193f4d62
ixmjst <tjosesimao@gmail.com>
2026-08-31 16:42:33 +0100

    part.3 do exercicio 3 concluida

--- exercicio-2.py
+++ exercicio-2.py
@@ -10,15 +10,23 @@ else:
     print("Igual a 0")
 
 # 2.Peça uma nota de 0 a 20 e classifique: reprovado,suficiente , bom ou excelente.
-    nota=float(input(" Digite uma nota de 0-20: "))
-    if(nota>16):
+nota=float(input(" Digite uma nota de 0-20: "))
+if(nota>16):
         print("Excelente")        
-    elif((nota>=13)and (nota<=16)):
+elif((nota>=13)and (nota<=16)):
         print("Bom")     
-    elif((nota>=10)and(nota<=12)):
+elif((nota>=10)and(nota<=12)):
         print("Suficiente")     
-    else:
+else:
         print("Reprovado")
 
-        
+#3.Imprimir "processando" se a var texto não estiver vazia e uma variavel de click for verdadeira.
+
+texto=""
+click=33
+texto=input("Digite um texto qualquer:")
+if ((texto!="")and (click)):
+    print("\bProcessando...")
+else:
+    print("nada para mostrar")
 
