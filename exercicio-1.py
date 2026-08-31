a8bb5fb3d49900d58085c3738b5c1fcda8fdedbf
ixmjst <tjosesimao@gmail.com>
2026-08-31 15:53:00 +0100

    Implementando a logica: Calcular em quantos anos a pessoa fara 100 anos

--- exercicio-1.py
+++ exercicio-1.py
@@ -1,8 +1,8 @@
- #Exercicio 1 : 
+ #Exercicio 1 : Var, tipos e entrada / saida 
  #Peça o nome e a idadae do usuário com input() e imprima uma frase que combine os dois.
- 
+ # Calcular em  quantos anos a pessoa fará 100 anos .
 nome = input("Digite o nome:").strip().lower()
 idade= int(input(" Digite a idade:"))
 
-print(f"Seu Nome:{nome}, e sua idade: {idade}")
+print(f"Seu Nome:{nome}, e sua idade: {idade}.Em {100-idade} fara 100 anos")
 
