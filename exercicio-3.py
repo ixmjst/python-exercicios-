b413e2958f18cfae904004a41ab21dedae815e97
ixmjst <tjosesimao@gmail.com>
2026-08-31 16:55:47 +0100

    part2  do exercicio 3 concluida

--- exercicio-3.py
+++ exercicio-3.py
@@ -7,3 +7,11 @@ for i in range(1,10):
 for j in range(1,10):
      par=f"{j}"if (j%2==0) else "numero impar"
      print(par)
+
+# 2. Some todos os  números de 1 a 100 usando um laço e uma variável acumuladora
+soma =0
+for k in range(1,100):
+    soma=soma+k
+
+print(f"Soma:{soma}")
+
