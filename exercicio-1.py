9358b4ab0e750b9b60d79385b2fb6c50c744bd9f
ixmjst <tjosesimao@gmail.com>
2026-08-31 16:02:51 +0100

    Mostrando o tipo de cada variavel criada

--- exercicio-1.py
+++ exercicio-1.py
@@ -1,8 +1,15 @@
  #Exercicio 1 : Var, tipos e entrada / saida 
  #Peça o nome e a idadae do usuário com input() e imprima uma frase que combine os dois.
- # Calcular em  quantos anos a pessoa fará 100 anos .
 nome = input("Digite o nome:").strip().lower()
 idade= int(input(" Digite a idade:"))
-
-print(f"Seu Nome:{nome}, e sua idade: {idade}.Em {100-idade} fara 100 anos")
+print(f"Seu Nome:{nome}, e sua idade: {idade}.")
+ # Calcular em  quantos anos a pessoa fará 100 anos . Mostre o resultado numa frase
+print(f"Em {100-idade} fara 100 anos ")
+#Crie variáveis com seu tipo: um texto (str), um inteiro (int), um decimal (float) e um
+#verdadeiro/falso (bool). Imprima o type() de cada uma para ver os nomes que o Python dá a eles
+texto ="ola, malta "
+inteiro=23
+decimal=34.3
+verdadeiro=False 
+print(f"Texto:{type(texto)},\nInterio:{type(inteiro)},\nDecimal:{type(decimal)},\nBooleano:{type(verdadeiro)}")
 
