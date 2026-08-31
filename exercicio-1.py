b2f8cbb7b8c6d2156c9f0429e3f342b9e34c5bd3
ixmjst <tjosesimao@gmail.com>
2026-08-31 15:47:47 +0100

    Exercicio 1 concluído

--- exercicio-1.py
+++ exercicio-1.py
@@ -1,1 +1,8 @@
+ #Exercicio 1 : 
+ #Peça o nome e a idadae do usuário com input() e imprima uma frase que combine os dois.
  
+nome = input("Digite o nome:").strip().lower()
+idade= int(input(" Digite a idade:"))
+
+print(f"Seu Nome:{nome}, e sua idade: {idade}")
+
