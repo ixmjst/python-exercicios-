141e6dafd2391f60459d271c4b1a79963b3b9d31
ixmjst <tjosesimao@gmail.com>
2026-08-31 16:11:47 +0100

    part. exercicio 2 concluida

--- exercicio-1.py
+++ exercicio-1.py
@@ -1,11 +1,11 @@
  #Exercicio 1 : Var, tipos e entrada / saida 
- #Peça o nome e a idadae do usuário com input() e imprima uma frase que combine os dois.
+ #1.Peça o nome e a idadae do usuário com input() e imprima uma frase que combine os dois.
 nome = input("Digite o nome:").strip().lower()
 idade= int(input(" Digite a idade:"))
 print(f"Seu Nome:{nome}, e sua idade: {idade}.")
- # Calcular em  quantos anos a pessoa fará 100 anos . Mostre o resultado numa frase
+ # 2.Calcular em  quantos anos a pessoa fará 100 anos . Mostre o resultado numa frase
 print(f"Em {100-idade} fara 100 anos ")
-#Crie variáveis com seu tipo: um texto (str), um inteiro (int), um decimal (float) e um
+#3.Crie variáveis com seu tipo: um texto (str), um inteiro (int), um decimal (float) e um
 #verdadeiro/falso (bool). Imprima o type() de cada uma para ver os nomes que o Python dá a eles
 texto ="ola, malta "
 inteiro=23
---
+++ exercicio-2.py
@@ -0,0 +1,12 @@
+# Exercicio 2 : Condicionais (if/elif/else )
+# Peça um número e diga se é positivo , negativo ou zero
+
+numero=int(input(" Digite um numero:"))
+if(numero>0):
+    print("Positivo")
+elif(numero<0):
+    print("Negativo")
+else:
+    print("Igual a 0")
+
+# 2
