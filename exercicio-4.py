5dbfd96af0fc860284fa49348d357fcaa3ca4510
ixmjst <tjosesimao@gmail.com>
2026-09-02 15:51:13 +0100

    exercicio 4 part 2 concluida

--- exercicio-4.py
+++ exercicio-4.py
@@ -1,6 +1,11 @@
 #Exercicio 4:Strings 
-#Peça uma frase e imprima: quantos caracteres tem, ela toda em maiusculas, e ela invertida.
+#1Peça uma frase e imprima: quantos caracteres tem, ela toda em maiusculas, e ela invertida.
 texto=input( "Digite uma frase :")
 print(f"Maiusculas:{texto.upper()},Tamanho:{len(texto)},Invertida:{texto[::-1]}")
 
+#2.Conte quantas palavaras há numa frase .
+
+frase= "O Jovem ficou insatisfeito"
+lista_de_palavras = frase.split()
+print(f"Tamanho da frase {frase} :{len(lista_de_palavras)}")
 
