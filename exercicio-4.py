02f6c5a84a972ae38b9bb3139f456ad460fe4646
ixmjst <tjosesimao@gmail.com>
2026-09-02 16:00:27 +0100

    exercicio 4 part 3 concluida

--- exercicio-4.py
+++ exercicio-4.py
@@ -9,3 +9,11 @@ frase= "O Jovem ficou insatisfeito"
 lista_de_palavras = frase.split()
 print(f"Tamanho da frase {frase} :{len(lista_de_palavras)}")
 
+#3.Limpe um texto bagunçado: troque quebras de linha por espaçoes e remova espaços duplicados 
+texto_exemplo=f"""
+ Ola eu sou o Paulo e estou muito feliz por ver você
+ Nossa amizade sera muito boa e proveitosa porque gosto de tecnologia e tu tambem.
+ Odeio ficar sem fazer nada.
+"""
+#junta tudo novamente usando apenas um espaço.
+print(" ".join(texto_exemplo.replace("\n"," ").split()))
