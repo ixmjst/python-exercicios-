e771e0fecc366a7dc308690d60b34fbd0a3e87f0
ixmjst <tjosesimao@gmail.com>
2026-09-02 16:54:55 +0100

    exercicio 5 part 3  concluida

--- exercicio-5.py
+++ exercicio-5.py
@@ -1,11 +1,21 @@
 #Exercicio 5:
 # 1. Crie uma lista de compras . Adicione itens com .append(), remova um, e imprima a lista final e seu tamanho
-lista_compras=["arroz", "feijao","batata","banana","sapado","bolsa"]
+lista_compras=["arroz", "feijao","batata","banana","sapato","bolsa"]
 lista_compras.append("Manteiga")
 lista_compras.remove("arroz")
+maior=1
+menor=1
 #2. Percorra a lista com for e enumere cada item da lista
+#3. Descubra o maior e o menor número de uma lista de números, sem usar max()/min() — na lógica, com um laço.
 print("Lista Final:")
 for indice, compra in enumerate(lista_compras,1):
-    print(indice,compra)
-
+     maior=indice
+     if(indice>maior):
+       maior=indice
+     if(indice<menor):
+       menor=indice
+     print(indice,compra)
+   
 print(f"Tamanho da Lista:{len(lista_compras)}")
+print("Maior: ",maior)
+print("Menor: ",menor)
