f2bba9fa0a97f17dbfa732227892969c4a5e01da
ixmjst <tjosesimao@gmail.com>
2026-09-02 15:32:46 +0100

    exercicio 4 parte 1 concluida

---
+++ exercicio-4.py
@@ -0,0 +1,6 @@
+#Exercicio 4:Strings 
+#Peça uma frase e imprima: quantos caracteres tem, ela toda em maiusculas, e ela invertida.
+texto=input( "Digite uma frase :")
+print(f"Maiusculas:{texto.upper()},Tamanho:{len(texto)},Invertida:{texto[::-1]}")
+
+
